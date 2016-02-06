921586576f5f8f68646009e0d0fe4935d42a3969
Egga (Jan Hartung) <github@egga.de>
2016-02-06 23:43:14 +0100

    the easy part

--- src/main/java/de/egga/bowling/Game.java
+++ src/main/java/de/egga/bowling/Game.java
@@ -1,4 +1,15 @@
 package de.egga.bowling;
 
 public class Game {
+
+    private int score = 0;
+
+    public void roll(int numberOfPins) {
+        score += numberOfPins;
+    }
+
+    public int score() {
+
+        return score;
+    }
 }
