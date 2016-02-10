fdeb292a09ce809de1f477a8b8cc844b6f080d3e
Egga (Jan Hartung) <github@egga.de>
2016-02-10 21:41:26 +0100

    nuke

--- src/main/java/de/egga/bowling/Game.java
+++ src/main/java/de/egga/bowling/Game.java
@@ -1,39 +1,5 @@
 package de.egga.bowling;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Game {
 
-    private List<Frame> frames = new ArrayList<Frame>();
-
-    public Game() {
-        frames.add(new Frame());
-    }
-
-    public void roll(int numberOfPins) {
-        startNewFrameIfNecessary();
-        currentFrame().addRoll(numberOfPins);
-    }
-
-    public int score() {
-        int score = 0;
-
-        for (Frame frame : frames) {
-            score += frame.score();
-        }
-
-        return score;
-    }
-
-
-    private void startNewFrameIfNecessary() {
-        if (currentFrame().isComplete()) {
-            frames.add(new Frame());
-        }
-    }
-
-    private Frame currentFrame() {
-        return frames.get(frames.size() - 1);
-    }
 }
--- src/test/java/de/egga/bowling/GameTest.java
+++ src/test/java/de/egga/bowling/GameTest.java
@@ -1,31 +1,6 @@
 package de.egga.bowling;
 
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class GameTest {
 
-    Game game = new Game();
 
-    @Test
-    public void score_should_be_zero_if_all_pins_missed() {
-        game.roll(0);
-        assertThat(game.score()).isEqualTo(0);
-    }
-
-    @Test
-    public void score_should_return_the_number_of_knocked_down_pins() {
-        game.roll(1);
-        assertThat(game.score()).isEqualTo(1);
-    }
-
-    @Test
-    public void score_should_return_the_sum_of_knocked_down_pins() {
-        game.roll(1);
-        game.roll(2);
-        game.roll(3);
-        game.roll(4);
-        assertThat(game.score()).isEqualTo(1+2+3+4);
-    }
 }
