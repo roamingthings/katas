407c4d5a21abd189ff60b0b30e8a44f95cc71290
Egga (Jan Hartung) <github@egga.de>
2016-02-11 22:49:57 +0100

    prototype. problem can be solved.
    refactoring and add error handling are left as exercise for the reader

--- src/test/java/de/egga/bowling/GameTest.java
+++ src/test/java/de/egga/bowling/GameTest.java
@@ -2,13 +2,30 @@ package de.egga.bowling;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GameTest {
 
     Game game = new Game();
 
     @Test
-    public void asd() {
+    public void score_should_be_zero_before_first_roll() {
+        int score = game.score();
+        assertThat(score).isEqualTo(0);
+    }
 
+    @Test
+    public void score_should_be_zero_after_failed_roll() {
+        game.roll(0);
+        int score = game.score();
+        assertThat(score).isEqualTo(0);
+    }
+
+    @Test
+    public void score_should_be_one() {
+        game.roll(1);
+        int score = game.score();
+        assertThat(score).isEqualTo(1);
     }
 
 }
