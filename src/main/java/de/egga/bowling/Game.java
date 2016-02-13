8759540222a0ea9ad4b96fefb07adcfea1fa5e5c
Egga (Jan Hartung) <github@egga.de>
2016-02-13 23:46:39 +0100

    restart bowling

--- src/main/java/de/egga/bowling/Game.java
+++ src/main/java/de/egga/bowling/Game.java
@@ -1,19 +1,6 @@
 package de.egga.bowling;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Game {
 
-    private List<Integer> rolls = new ArrayList<Integer>();
-
-    public void roll(int i) {
-        rolls.add(i);
-    }
-
-    public int score() {
-        List<Integer> rolls = this.rolls;
-        return ScoreCalculator.calc(rolls);
-    }
 
 }
--- src/test/java/de/egga/bowling/GameTest.java
+++ src/test/java/de/egga/bowling/GameTest.java
@@ -1,31 +1,7 @@
 package de.egga.bowling;
 
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class GameTest {
 
     Game game = new Game();
-
-    @Test
-    public void score_should_be_zero_before_first_roll() {
-        int score = game.score();
-        assertThat(score).isEqualTo(0);
-    }
-
-    @Test
-    public void score_should_be_zero_after_failed_roll() {
-        game.roll(0);
-        int score = game.score();
-        assertThat(score).isEqualTo(0);
-    }
-
-    @Test
-    public void score_should_be_one() {
-        game.roll(1);
-        int score = game.score();
-        assertThat(score).isEqualTo(1);
-    }
 
 }
