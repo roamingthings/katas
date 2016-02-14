04a9bfa579540dc290982563782ed0e7c9436dc6
Egga (Jan Hartung) <github@egga.de>
2016-02-14 20:36:09 +0100

    niuke the bowl again

--- src/test/java/de/egga/bowling/GameTest.java
+++ src/test/java/de/egga/bowling/GameTest.java
@@ -1,23 +1,5 @@
 package de.egga.bowling;
 
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class GameTest {
 
-    Game game = new Game();
-
-    @Test
-    public void it_should_have_zero_score_before_first_roll() {
-        int score  = game.score();
-        assertThat(score).isEqualTo(0);
-    }
-
-    @Test
-    public void it_should_have_score_of_two_after_according_roll() {
-        game.roll(2);
-        int score  = game.score();
-        assertThat(score).isEqualTo(2);
-    }
 }
