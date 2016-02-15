f872157096ad700d81f561f8b098697a8c1b9ce3
Egga (Jan Hartung) <github@egga.de>
2016-02-15 20:27:05 +0100

    bowling solution

--- src/main/java/de/egga/bowling/Game.java
+++ src/main/java/de/egga/bowling/Game.java
@@ -1,5 +1,97 @@
 package de.egga.bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
+    private int multiplier = 0;
+    private List<Frame> frames = new ArrayList<>();
+    private Frame currentFrame;
+
+    public Game() {
+        addNewFrame();
+    }
+
+    public void roll(int numberOfPins) {
+        createNewFrameIfNecessary();
+        currentFrame.addRoll(numberOfPins);
+    }
+
+    private void createNewFrameIfNecessary() {
+        if (currentFrame.isFinished()) {
+            if (currentFrame.isSpare()) {
+                multiplier = multiplier + 1;
+            }
+
+            if (currentFrame.isStrike()) {
+                multiplier = multiplier + 1;
+            }
+
+            addNewFrame();
+            multiplier = 0;
+        }
+    }
+
+    private void addNewFrame() {
+        Frame newFrame = new Frame(multiplier);
+        frames.add(newFrame);
+        currentFrame = newFrame;
+    }
+
+    public int score() {
+        int sum = 0;
+
+        for (Frame frame : frames) {
+            sum += frame.score();
+        }
+
+        return sum;
+    }
+
+    private class Frame {
+
+        public static final int TOTAL_NUMBER_OF_PINS = 10;
+        public static final int ROLLS_PER_FRAME = 2;
+
+        private int pinsLeft = TOTAL_NUMBER_OF_PINS;
+        private int rollsLeft = ROLLS_PER_FRAME;
+        private List<Integer> pinsKnockedDown = new ArrayList<>();
+        private int bonusRolls;
+
+        public Frame(int bonusRolls) {
+            this.bonusRolls = bonusRolls;
+        }
+
+        public void addRoll(int numberOfPins) {
+            pinsKnockedDown.add(numberOfPins);
+            pinsLeft -= numberOfPins;
+            rollsLeft--;
+        }
+
+        public int score() {
+            int score = 0;
+            for (Integer integer : pinsKnockedDown) {
+                if (bonusRolls > 0) {
+                    score += integer;
+                    bonusRolls--;
+                }
+                score += integer;
+            }
+
+            return score;
+        }
+
+        public boolean isFinished() {
+            return rollsLeft == 0 || pinsLeft == 0;
+        }
+
+        public boolean isSpare() {
+            return pinsLeft == 0 && rollsLeft == 0;
+        }
+
+        public boolean isStrike() {
+            return pinsLeft == 0 && rollsLeft == 1;
+        }
+    }
 }
--- src/test/java/de/egga/bowling/GameTest.java
+++ src/test/java/de/egga/bowling/GameTest.java
@@ -1,7 +1,49 @@
 package de.egga.bowling;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GameTest {
 
     Game game = new Game();
 
+    @Test
+    public void it_should_have_score_of_zero_before_first_r0ll() {
+        assertThat(game.score()).isEqualTo(0);
+    }
+
+    @Test
+    public void it_should_have_score_of_zero_if_no_pins_knocked_down() {
+        game.roll(0);
+        assertThat(game.score()).isEqualTo(0);
+    }
+
+    @Test
+    public void it_should_have_score_according_to_pins_knocked_down() {
+        game.roll(1);
+        game.roll(2);
+        game.roll(3);
+        assertThat(game.score()).isEqualTo(1 + 2 + 3);
+    }
+
+    @Test
+    public void it_should_add_bonus_for_spare() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(2);
+        game.roll(3);
+        game.roll(4);
+        assertThat(game.score()).isEqualTo(5 + 5 + (2 * 2) + 3 + 4);
+    }
+
+    @Test
+    public void it_should_add_bonus_for_strike() {
+        game.roll(10);
+        game.roll(2);
+        game.roll(2);
+        game.roll(3);
+        game.roll(4);
+        assertThat(game.score()).isEqualTo(10 + (2 * 2 + 2) + 3 + 4);
+    }
 }
