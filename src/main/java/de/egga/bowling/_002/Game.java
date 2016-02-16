cfcdc5bc3b4f7c02dea13280be1c9ec808134cc4
Egga (Jan Hartung) <github@egga.de>
2016-02-16 23:23:15 +0100

    the easy part

--- src/main/java/de/egga/bowling/_002/Game.java
+++ src/main/java/de/egga/bowling/_002/Game.java
@@ -1,7 +1,23 @@
 package de.egga.bowling._002;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private List<Integer> rolls = new ArrayList<>();
+
+    public void roll(int numberOfPins) {
+        rolls.add(numberOfPins);
+    }
+
     public int score() {
-        return 0;
+        int sum = 0;
+
+        for (Integer roll : rolls) {
+            sum += roll;
+        }
+
+        return sum;
     }
 }
--- src/test/java/de/egga/bowling/_002/GameTest.java
+++ src/test/java/de/egga/bowling/_002/GameTest.java
@@ -9,7 +9,21 @@ public class GameTest {
     Game game = new Game();
 
     @Test
-    public void itName() {
+    public void it_should_have_score_of_zero_before_first_roll() {
         assertThat(game.score()).isEqualTo(0);
+    }
+
+    @Test
+    public void it_should_have_score_of_zero_if_no_pins_knocked_down() {
+        game.roll(0);
+        assertThat(game.score()).isEqualTo(0);
+    }
+
+    @Test
+    public void it_should_add_pins_as_score() {
+        game.roll(1);
+        game.roll(2);
+        game.roll(3);
+        assertThat(game.score()).isEqualTo(1+2+3);
     }
 }
