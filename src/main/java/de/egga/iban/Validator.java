ec9edd462e3c8967fb99e2e87a30942c3388b26d
Egga (Jan Hartung) <github@egga.de>
2016-03-30 18:13:16 +0200

    iban kata: 1-1

---
+++ src/main/java/de/egga/iban/Validator.java
@@ -0,0 +1,16 @@
+package de.egga.iban;
+
+public class Validator {
+
+
+    public static final int MAX_IBAN_LENGTH = 34;
+
+    public static boolean isIban(String iban) {
+
+        if (iban.length() > MAX_IBAN_LENGTH) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/de/egga/iban/ValidatorTest.java
@@ -0,0 +1,33 @@
+package de.egga.iban;
+
+import org.junit.Test;
+
+import static de.egga.iban.Validator.isIban;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidatorTest {
+
+    @Test
+    public void less_than_34_chars_should_be_valid() {
+        String shortString = "some iban";
+        assertThat(isIban(shortString)).isTrue();
+    }
+
+    @Test
+    public void exactly_34_chars_should_be_valid() {
+        String stringWith34Chars = "some iban that is exactly 34 chars";
+        assertThat(isIban(stringWith34Chars)).isTrue();
+    }
+
+    @Test
+    public void exactly_35_chars_should_not_be_valid() {
+        String stringWith35Chars = "some iban that is exactly 35 chars.";
+        assertThat(isIban(stringWith35Chars)).isFalse();
+    }
+
+    @Test
+    public void more_than_35_chars_should_not_be_valid() {
+        String stringWithMoreChars = "some iban that is far more 35 chars.";
+        assertThat(isIban(stringWithMoreChars)).isFalse();
+    }
+}
