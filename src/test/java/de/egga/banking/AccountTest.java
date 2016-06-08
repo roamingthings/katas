9da699146974a31d2fcde4b3ca64d99478f66b91
Egga (Jan Hartung) <github@egga.de>
2016-06-08 21:36:12 +0200

    banking: clean up

--- src/test/java/de/egga/banking/AccountTest.java
+++ src/test/java/de/egga/banking/AccountTest.java
@@ -2,14 +2,11 @@ package de.egga.banking;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class AccountTest {
 
     @Test
     public void itName() {
         Account account = new Account();
         account.printStatement();
-        assertThat("").isEqualTo("");
     }
 }
