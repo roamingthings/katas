13d9c25f17cae745d865fc9c439e8a195eaaca5c
Egga (Jan Hartung) <github@egga.de>
2016-06-08 21:55:26 +0200

    banking: add equals

--- src/main/java/de/egga/banking/Transaction.java
+++ src/main/java/de/egga/banking/Transaction.java
@@ -2,7 +2,25 @@ package de.egga.banking;
 
 public class Transaction {
 
-    public Transaction(int amount) {
+    private final int amount;
 
+    public Transaction(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        return amount == that.amount;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return amount;
     }
 }
--- src/test/java/de/egga/banking/TransactionTest.java
+++ src/test/java/de/egga/banking/TransactionTest.java
@@ -2,10 +2,13 @@ package de.egga.banking;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TransactionTest {
 
     @Test
     public void itName() {
-        new Transaction(12);
+        Transaction transaction = new Transaction(12);
+        assertThat(transaction).isEqualTo(new Transaction(12));
     }
 }
