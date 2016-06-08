ff3a62d9f5b3129cb9dfb0e22cf7c9c6610a278b
Egga (Jan Hartung) <github@egga.de>
2016-06-08 21:56:14 +0200

    banking: refactor

--- src/test/java/de/egga/banking/TransactionTest.java
+++ src/test/java/de/egga/banking/TransactionTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TransactionTest {
 
     @Test
-    public void itName() {
+    public void transactions_with_same_amount_should_be_equal() {
         Transaction transaction = new Transaction(12);
         assertThat(transaction).isEqualTo(new Transaction(12));
     }
