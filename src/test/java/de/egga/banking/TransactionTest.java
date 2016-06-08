18311bf280276069feb0170b9721d8ae43290a41
Egga (Jan Hartung) <github@egga.de>
2016-06-08 21:57:07 +0200

    banking: add non equal test. TRIANGULATION

--- src/test/java/de/egga/banking/TransactionTest.java
+++ src/test/java/de/egga/banking/TransactionTest.java
@@ -11,4 +11,10 @@ public class TransactionTest {
         Transaction transaction = new Transaction(12);
         assertThat(transaction).isEqualTo(new Transaction(12));
     }
+
+    @Test
+    public void transactions_with_different_amount_should_not_be_equal() {
+        Transaction transaction = new Transaction(12);
+        assertThat(transaction).isNotEqualTo(new Transaction(34));
+    }
 }
