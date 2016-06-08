e04d5924a9db8b666379210475b0ad7ca88a5f3f
Egga (Jan Hartung) <github@egga.de>
2016-06-08 21:39:14 +0200

    banking: refactor

--- src/test/java/de/egga/banking/AccountTest.java
+++ src/test/java/de/egga/banking/AccountTest.java
@@ -1,13 +1,20 @@
 package de.egga.banking;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AccountTest {
+
+    @Mock Printer printer;
+    @InjectMocks Account account;
 
     @Test
     public void itName() {
-        Printer printer = new Printer();
-        Account account = new Account(printer);
         account.printStatement();
+
     }
 }
