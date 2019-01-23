9866ce1d85397fc50ae2f072f4d764e7e5011c80
Alexander Sparkowsky <info@roamingthings.de>
2019-01-23 14:48:23 +0100

    Gilded Rose Kotlin: Increment quality as extension function

--- modules/gilded_rose/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ modules/gilded_rose/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,18 +6,18 @@ class GildedRose(var items: Array<Item>) {
         for (item in items) {
             if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.incrementQuality()
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.incrementQuality()
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.incrementQuality()
                             }
                         }
                     }
@@ -37,12 +37,12 @@ class GildedRose(var items: Array<Item>) {
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.incrementQuality()
                     }
                 } else {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = item.quality - item.quality
-
+                    } else {
                         if (item.quality > 0) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                                 item.quality = item.quality - 1
@@ -54,4 +54,8 @@ class GildedRose(var items: Array<Item>) {
         }
     }
 
+}
+
+fun Item.incrementQuality() {
+    this.quality++
 }
