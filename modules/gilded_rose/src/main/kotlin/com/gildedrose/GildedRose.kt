f067d6f2fa06f5364da2a92852106a54f2743ebc
Alexander Sparkowsky <info@roamingthings.de>
2019-01-23 15:19:25 +0100

    Gilded Rose Kotlin: Simplify if expressions according Kotlin style guide

--- modules/gilded_rose/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ modules/gilded_rose/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,21 +6,18 @@ class GildedRose(var items: Array<Item>) {
          for (item in items) when {
              item.isBrie() -> updateBrie(item)
              item.isBackstage() -> updateBackstage(item)
-             item.isSulfuras() -> {}    // Sulfuras does not age
+             item.isSulfuras() -> {}    // Sulfuras does not age nor decrease in quality
              else -> updateOrdinary(item)
          }
     }
 
     private fun updateOrdinary(item: Item) {
-        if (item.quality > 0) {
-            item.decrementQuality()
-        }
+
+        if (item.quality > 0) item.decrementQuality()
 
         item.decrementSellIn()
 
-        if (item.sellIn < 0 && item.quality > 0) {
-            item.decrementQuality()
-        }
+        if (item.sellIn < 0 && item.quality > 0) item.decrementQuality()
     }
 
 
@@ -30,27 +27,20 @@ class GildedRose(var items: Array<Item>) {
 
         item.decrementSellIn()
 
-        if (item.sellIn < 0) {
-            item.incrementQuality()
-        }
+        if (item.sellIn < 0) item.incrementQuality()
     }
 
     private fun updateBackstage(item: Item) {
+
         item.incrementQuality()
 
-        if (item.sellIn < 11) {
-            item.incrementQuality()
-        }
+        if (item.sellIn < 11) item.incrementQuality()
 
-        if (item.sellIn < 6) {
-            item.incrementQuality()
-        }
+        if (item.sellIn < 6) item.incrementQuality()
 
         item.decrementSellIn()
 
-        if (item.sellIn < 0) {
-            item.quality = 0
-        }
+        if (item.sellIn < 0) item.quality = 0
     }
 
 }
